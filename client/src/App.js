acede3aaf3d359e5785e053a0feaf821c3deb078
Griffin <griffincriste@gmail.com>
2019-09-18 09:40:35 -0500

    stuff

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+          <Route exact path="/saved/:id" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import SavedResult from "../components/SavedList"
+
+class Saved extends Component {
+  state = {
+      savedBooks: []
+  };
+
+  //when this component mounts, grab all books that were save to the database 
+  componentDidMount() {
+      API.getBooks()
+          .then(res => this.setState({ savedBooks: res.data }))
+          .catch(err => console.log(err))
+  }
+
+  //function to remove book by id
+  handleDeleteButton = id => {
+      API.deleteBook(id)
+          .then(res => this.componentDidMount())
+          .catch(err => console.log(err))
+  }
+
+  render() {
+      return (
+          <Container fluid className="container">
+              <Jumbotron />
+              <Container>
+                  <SavedResult savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
+              </Container>
+          </Container>
+      )
+  }
+}
+
+
+
+export default Saved;
