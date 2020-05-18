a0f6c0f0e1662f60b11bfedd85dbc4d9f922fc67
Griffin <griffincriste@gmail.com>
2020-05-18 16:30:16 -0500

    work

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
