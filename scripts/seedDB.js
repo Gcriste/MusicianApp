1d06dab5e64a0bc2892848e0a80c38ee610289f9
Griffin <griffincriste@gmail.com>
2020-05-18 16:35:10 -0500

    heroku not working

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,30 +1,30 @@
-const mongoose = require("mongoose");
-const db = require("../models");
+// const mongoose = require("mongoose");
+// const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
+// // This file empties the Books collection and inserts the books below
 
-mongoose.connect(
-  process.env.MONGODB_URI 
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI 
+// );
 
-const gigSeed = {
-  pay: [300],
-  venue: "Tootsies",
-  bandname: "Maddie in Good Company",
-    musictype: "funk soul rock",
-  date: "10/18/2019",
-  time: "6:30-9:30pm"
+// const gigSeed = {
+//   pay: [300],
+//   venue: "Tootsies",
+//   bandname: "Maddie in Good Company",
+//     musictype: "funk soul rock",
+//   date: "10/18/2019",
+//   time: "6:30-9:30pm"
 
-}
+// }
 
-db.Gig
-  .remove({})
-  .then(() => db.Gig.collection.insertMany(gigSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+// db.Gig
+//   .remove({})
+//   .then(() => db.Gig.collection.insertMany(gigSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
